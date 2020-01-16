36bd28702f1b3695ba941492e9619acd10f693c9
Mohamed Atef <proofesser@gmail.com>
2020-01-16 03:25:36 -0800

    get comments successful parsed using GSON

--- app/src/main/java/mo/atef/retrofit/example/retrofitandroidapp/MainActivity.java
+++ app/src/main/java/mo/atef/retrofit/example/retrofitandroidapp/MainActivity.java
@@ -31,6 +31,39 @@ public class MainActivity extends AppCompatActivity {
         jsonPlaceHolderApi=retrofit.create(JsonPlaceHolderApi.class);
 
         //getPosts();
+
+        getComments();
+    }
+
+    private void getComments() {
+        Call<List<Comment>> call=jsonPlaceHolderApi.getComments();
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+                if (!response.isSuccessful()){
+                    textViewResult.setText("code: "+response.code());
+                    return;
+                }
+
+                List<Comment> comments=response.body();
+
+                for (Comment comment:comments){
+                    String content="";
+                    content+="ID: "+comment.getId()+"\n";
+                    content+="post ID: "+comment.getPostId()+"\n";
+                    content+="Name: "+comment.getName()+"\n";
+                    content+="Email: "+comment.getEmail()+"\n";
+                    content+="text: "+comment.getText()+"\n\n";
+
+                    textViewResult.append(content );
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
     }
 
     private void getPosts() {
