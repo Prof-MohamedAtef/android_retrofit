b966d88110dc31bad04664f6d2bbdbd684eb1d0e
Mohamed Atef <proofesser@gmail.com>
2020-01-17 00:28:46 -0800

    commented FormUrlEncoded Fields and passed FormUrlEncoded FieldMap (HashMap) ...

--- app/src/main/java/mo/atef/retrofit/example/retrofitandroidapp/JsonPlaceHolderApi.java
+++ app/src/main/java/mo/atef/retrofit/example/retrofitandroidapp/JsonPlaceHolderApi.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
@@ -48,5 +49,11 @@ public interface JsonPlaceHolderApi {
             @Field("userId") int userId,
             @Field("title") String title,
             @Field("body") String text
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<Post> createPost(
+            @FieldMap Map<String, String> fields
     );
 }
